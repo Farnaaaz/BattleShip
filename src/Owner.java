828fbd259d902960b32b264b74c818f33ec93836
FarnaZ <74882606+Farnaaaz@users.noreply.github.com>
2021-01-06 20:33:10 -0500

    Update Owner.java

--- src/Owner.java
+++ src/Owner.java
@@ -1,8 +1,4 @@
 //***************************************************************************************************************
-// Assignment 4 
-// Written by: Farnaz Zaveh, ID: 40032389
-// For COMP 248-R - Fall 2020  
-//
 // This program uses an array of object and includes four classes and one driver class to simulate the battleship game between a human user and the computer
 //This class include a class with all possible values for the owner of each cell 
 //***************************************************************************************************************
